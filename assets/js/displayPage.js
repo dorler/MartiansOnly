7dccb86caf4c7bff3ef935012924189816be7320
unknown <michael_pnkstn@yahoo.com>
2021-06-01 15:53:40 -0400

    add api for pictures and weather

--- assets/js/displayPage.js
+++ assets/js/displayPage.js
@@ -4,14 +4,43 @@ var destination = document.querySelector("#destination");
 var departDate = document.querySelector("#dateSelector");
 
 
-function getMarsInfo() {
+function getMarsPics() {
 
+    var picsApi = "https://api.nasa.gov/mars-photos/api/v1/rovers/" + destination.value + "/photos?sol=1000&camera=fhaz&api_key=gx8qpDn3blyfVIl6JTpYJUW5ddk4mpO1cUgBqvKO";
 
+    fetch(picsApi).then(function(response) {
 
-
+        if(response.ok) {
+            response.json().then(function(data) {
+                console.log(response);
+                console.log(data);
+            })
+        } else {
+            alert("Error: " + response.statusText)
+        };
+    });
 
 };
 
+/*
+function getMarsWeather() {
+
+    var weatherApi = "https://api.nasa.gov/insight_weather/?api_key=gx8qpDn3blyfVIl6JTpYJUW5ddk4mpO1cUgBqvKO&feedtype=json&ver=1.0";
+
+    fetch(weatherApi).then(function(response) {
+
+        if(response.ok) {
+            response.json().then(function(data) {
+                console.log(response);
+                console.log(data);
+            })
+        } else {
+            alert("Error: " + response.statusText)
+        };
+    });
+
+};
+*/
 
 
 
@@ -35,6 +64,4 @@ function getMarsInfo() {
 
 
 
-
-
-searchFlight.addEventListener("click", getMarsInfo());
+searchFlight.addEventListener("click", getMarsPics);
