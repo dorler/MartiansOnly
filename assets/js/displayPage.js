60f2444d1a28214494c16569425e8a63aafc0d4d
unknown <michael_pnkstn@yahoo.com>
2021-06-05 16:29:51 -0400

    price generation and link index and bookFlight

--- assets/js/displayPage.js
+++ assets/js/displayPage.js
@@ -2,7 +2,20 @@ var searchFlight = document.querySelector("#search-flight");
 var departFrom = document.querySelector("#departure-option");
 var destination = document.querySelector("#destination");
 var departDate = document.querySelector("#dateSelector");
+var priceSpaceX = document.querySelector("#priceSpaceX");
+var priceNASA = document.querySelector("#priceNASA");
 
+
+function priceGen() {
+    var priceOne = Math.floor(Math.random() * 100000 + 100000)
+
+    priceSpaceX.innerHTML = "$" + priceOne
+
+    var priceTwo = Math.floor(Math.random() * 100000 + 100000)
+
+    priceNASA.innerHTML = "$" + priceTwo
+    
+}
 
 function getMarsInfo() {
 
@@ -36,5 +49,5 @@ function getMarsInfo() {
 
 
 
-
+priceGen();
 searchFlight.addEventListener("click", getMarsInfo());
